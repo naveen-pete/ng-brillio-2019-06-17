ca3923b46f5cf0158b21a8309bee2093d1fa8016
naveen-pete <naveen.shenoy.blr@gmail.com>
2019-06-18 18:00:53 +0530

    commit store app at the end of day 2

--- apps/day-02/store-app/src/app/app.component.ts
+++ apps/day-02/store-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',                // required
+  templateUrl: './app.component.html', // required, external template
+  styleUrls: ['./app.component.css']   // optional, external stylesheets
 })
 export class AppComponent {
-  title = 'store-app';
 }
